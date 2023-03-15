2b9515b4b7a368b20247333b481b99c8ce5a0933
manuelnones <manuelnones99@yahoo.com>
2023-03-15 17:47:59 +0100

    add function doneTask

--- js/script.js
+++ js/script.js
@@ -1,26 +1,36 @@
-const { createApp } = Vue
+const {
+    createApp
+} = Vue
 
-  createApp({
+createApp({
     data() {
-      return {
-        todos: [
-            {
-                text: `comprare il pane`,
-                done: false,
-            },
-            {
-                text: `prendere appuntamento`,
-                done: false,
-            },
-            {
-                text: `ordinare torta compleanno`,
-                done: false,
-            },
-            {
-                text: `lavare la macchina`,
-                done: false,
-            },
-        ]
-      }
+        return {
+            todos: [
+                {
+                    text: `comprare il pane`,
+                    done: false,
+                },
+                {
+                    text: `prendere appuntamento`,
+                    done: false,
+                },
+                {
+                    text: `ordinare torta compleanno`,
+                    done: false,
+                },
+                {
+                    text: `lavare la macchina`,
+                    done: false,
+                },
+            ]
+        }
+    },
+
+    methods: {
+        doneTask(index) {
+            this.todos.slice(index, 1)
+            console.log(`cliccato`)
+        }
     }
-  }).mount('#app')
+
+}).mount('#app')
